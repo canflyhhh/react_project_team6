ea4d47ff3efc0d4e0a15144645a79ee4014d7a6e
葉文心 <WenHsin0130@gmail.com>
2023-11-22 12:04:52 +0800

    Commit 主畫面

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,12 @@
 import * as React from 'react';
 import usePosts from './post/usePosts';
 import { useEffect } from 'react';
-import { Grid, Card, CardContent, Typography, CardActions, Button, Slide, 
-    TransitionProps, Dialog, DialogTitle, DialogActions, DialogContentText, DialogContent
-    , useMediaQuery } from "@mui/material";
+import { 
+    Grid, Card, CardContent, Typography, CardActions, Button, Slide, 
+    TransitionProps, Dialog, DialogTitle, DialogActions, DialogContentText, DialogContent , useMediaQuery 
+} from "@mui/material";
 import { useTheme } from '@mui/material/styles';
 import { useRouter } from 'next/navigation';
-
-
-// post view - slide in
-const Transition = React.forwardRef(function Transition(
-    props: TransitionProps & {
-      children: React.ReactElement<any, any>;
-    },
-    ref: React.Ref<unknown>,
-  ) {
-    return <Slide direction="up" ref={ref} {...props} />;
-});
-  
 
 
 export default function Home() {
@@ -27,7 +16,7 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <Grid container spacing={2}  sx={{ padding: 4 }}>
+    <Grid container spacing={2} sx={{ padding: 4 }}>
         {posts.map((post) => (
             <Grid item xs={4}>
                 <Card variant="outlined">
@@ -54,11 +43,12 @@ export default function Home() {
                         </Typography>
                     </CardContent>
                     <CardActions>
-                        <Button variant="outlined" onClick={() => router.push("/post_context")}>
+                        <Button variant="outlined" onClick={() => router.push(`/post_content`)}>
                             查看內容
                         </Button>
                     </CardActions>
                 </Card>
+                {post.id}
             </Grid>
         ))}
     </Grid>
