23b0dd391543bcf5048c0286df970ff293d2893f
canflyhhh <kathleen9009110911@gmail.com>
2023-11-22 11:49:17 +0800

    filter 校內/校外

--- app/filter/page.tsx
+++ app/filter/page.tsx
@@ -1,25 +1,50 @@
 'use client'
-import React from "react";
-import useProducts from "./test_page";
+import React, { useState } from "react";
+import usePosts from "./test_page";
+import { Grid, Card, CardContent, Typography, CardActions, Button } from "@mui/material";
 
-function ProductList() {
-  const [products, setProducts] = useProducts();
+function PostsList() {
+  const [tag, setTag] = useState("");
+  const [posts, setPosts] = usePosts(tag);
+  
+
+  const handleButtonClick = (selectedTag: string) => {
+    setTag(selectedTag);
+  };
 
   return (
     <div>
-      <h1>Product List</h1>
-      <ul>
-        {products.map((product, index) => (
-          <li key={index}>
-            <h2>{product.title}</h2>
-            <p>Account: {product.account}</p>
-            <p>Context: {product.context}</p>
-            <p>Time: {product.time.toDate().toLocaleString()}</p>
-          </li>
-        ))}
+      <button onClick={() => handleButtonClick("貿易")}>校內</button>
+      <button onClick={() => handleButtonClick("琥珀")}>校外</button>
+
+      <ul>   
+        <Grid container spacing={2} sx={{ padding: 4 }}>
+          {posts.map((posts, index) => (
+            <Grid item xs={4}>
+              <Card variant="outlined">
+                <li key={index}>
+                <CardContent>
+                  <Typography variant="h5" component="div">
+                    {posts.title}
+                  </Typography>
+                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    {posts.account}
+                  </Typography>
+                  <Typography variant="body2">{posts.context}</Typography>
+                    <p>Time: {posts.time.toDate().toLocaleString()}</p>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+                </li>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>  
       </ul>
     </div>
   );
 }
 
-export default ProductList;
+export default PostsList;
+
