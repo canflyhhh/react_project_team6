6620e9e320a7a350c114055089948cd5b3efba3c
WillieYu2000123 <willieyu2000123@gmail.com>
2023-12-15 14:46:18 +0800

    在/in_school路徑藉由quill可以讀取多種元素與圖片
    在/in_school路徑藉由quill可以讀取多種元素與圖片

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -28,45 +28,9 @@ import { Post } from "../_settings/interfaces";
 import AddIcon from '@mui/icons-material/Add';
 import { AuthContext } from '../account/authContext';
 
-
-
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-
-function MyTextEditor() {
-    const [value, setValue] = useState('');
-
-
-    const modules = {
-        toolbar: [
-            [{ 'header': [1, 2, false] }],
-            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-            ['link', 'image'],
-            ['clean']
-        ],
-    };
-
-    const formats = [
-        'header',
-        'bold', 'italic', 'underline', 'strike', 'blockquote',
-        'list', 'bullet', 'indent',
-        'link', 'image'
-    ];
-
-    return (
-        <div className="text-editor">
-            <ReactQuill
-                theme="snow"
-                value={value}
-                onChange={setValue}
-                modules={modules}
-                formats={formats}
-            />
-        </div>
-    );
-}
 
 
 interface TagInputProps {
