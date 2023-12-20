5c5df91528f393c245d28562b79affe92db1d41c
葉文心 <WenHsin0130@gmail.com>
2023-12-20 20:41:09 +0800

    Home page card

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import { useRouter } from 'next/navigation';
 
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import { CalendarMonth, Whatshot, TouchApp, AdsClick, AutoAwesome } from '@mui/icons-material';
+import { CalendarMonth, Whatshot, TouchApp, AdsClick, AutoAwesome, Margin } from '@mui/icons-material';
 
 
 export default function Home() {
@@ -34,7 +34,7 @@ export default function Home() {
         setStatus("詳細");
         setId(id);
     }
-    //從詳細資訊跳至總攬 
+    //從詳細資訊跳至總覽 
     function changeStatus(status: string) {
         if (status === "本月" || status === "熱門") {
             setStatus(status);
@@ -51,79 +51,71 @@ export default function Home() {
     function stripHtmlTags(html: string) {
         const doc = new DOMParser().parseFromString(html, 'text/html');
         return doc.body.textContent || "";
-    }
+    };
 
     // 熱門、最新文章 card（大）
     function postCard(post: { time: any; account: any; context: any; title: any; Id: any; }) {
         return (
-            <Grid item key={post.Id} sx={{ margin: '2em' }}>
-                <Card variant="outlined" sx={{ padding: '1em' }}>
-                    <CardContent>
-                        <Typography variant="h4" component="div" sx={{ marginY: 1 }}>
-                            {post.title}
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                            {post.account}
-                        </Typography>
-                        <Typography variant="body2">
-                            {post.context.length > 150
-                                ? `${stripHtmlTags(post.context).substring(0, 150)}……`
-                                : stripHtmlTags(post.context)
-                            }
-                        </Typography>
-                    </CardContent>
-                    <Divider light sx={{ margin: '1em' }} />
-                    <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Typography sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
-                            <CalendarMonth sx={{ fontSize: '1rem', marginRight: '0.2em' }} />
-                            {post.time.toDate().toLocaleString()}
-                        </Typography>
-                        <Button variant="outlined" onClick={() => detailContex(post.Id)} startIcon={<AdsClick />} size="large">
-                            查看內容
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
+            <Card variant="outlined" sx={{ padding: '1em' }}>
+                <CardContent>
+                    <Typography variant="h4" component="div" sx={{ marginY: 1 }}>
+                        {post.title}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {post.account}
+                    </Typography>
+                    <Typography variant="body2">
+                        {post.context.length > 150
+                            ? `${stripHtmlTags(post.context).substring(0, 150)}……`
+                            : stripHtmlTags(post.context)
+                        }
+                    </Typography>
+                </CardContent>
+                <Divider light sx={{ margin: '1em' }} />
+                <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
+                        <CalendarMonth sx={{ fontSize: '1rem', marginRight: '0.2em' }} />
+                        {post.time.toDate().toLocaleString()}
+                    </Typography>
+                    <Button variant="outlined" onClick={() => detailContex(post.Id)} startIcon={<AdsClick />} size="large">
+                        查看內容
+                    </Button>
+                </CardActions>
+            </Card>
         )
     }
 
     // 查看所有最新文章、所有熱門文章
     function smallPostCard(post: { time: any; account: any; context: any; title: any; Id: any; }) {
         return (
-            <Grid item key={post.Id} sx={{ margin: '0.5em' }}>
-                <Card variant="outlined" sx={{ padding: '1em' }}>
-                    <CardContent>
-                        <Typography variant="h6" component="div" sx={{ marginY: 1 }}>
-                            {post.title}
-                        </Typography>
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                            {post.account}
-                        </Typography>
-                        <Typography variant="body2">
-                            {post.context.length > 150
-                                ? `${stripHtmlTags(post.context).substring(0, 150)}……`
-                                : stripHtmlTags(post.context)
-                            }
-                        </Typography>
-                    </CardContent>
-                    <Divider light sx={{ margin: '0.5em' }} />
-                    <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                        <Typography sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
-                            <CalendarMonth sx={{ fontSize: '1rem', marginRight: '0.2em' }} />
-                            {post.time.toDate().toLocaleString()}
-                        </Typography>
-                        <Button variant="outlined" onClick={() => detailContex(post.Id)} startIcon={<AdsClick />} size="large">
-                            查看內容
-                        </Button>
-                    </CardActions>
-                </Card>
-            </Grid>
+            <Card variant="outlined" sx={{ padding: '1em' }}>
+                <CardContent>
+                    <Typography variant="h6" component="div" sx={{ marginY: 1 }}>
+                        {post.title}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        {post.account}
+                    </Typography>
+                    <Typography variant="body2">
+                        {post.context.length > 150
+                            ? `${stripHtmlTags(post.context).substring(0, 150)}……`
+                            : stripHtmlTags(post.context)
+                        }
+                    </Typography>
+                </CardContent>
+                <Divider light sx={{ margin: '0.5em' }} />
+                <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
+                        <CalendarMonth sx={{ fontSize: '1rem', marginRight: '0.2em' }} />
+                        {post.time.toDate().toLocaleString()}
+                    </Typography>
+                    <Button variant="outlined" onClick={() => detailContex(post.Id)} startIcon={<AdsClick />} size="large">
+                        查看內容
+                    </Button>
+                </CardActions>
+            </Card>
         )
     }
-
-
-
-
 
     // 換頁
     const [page, setPage] = useState(1);
@@ -131,7 +123,7 @@ export default function Home() {
 
     function handleChangePage(event: React.ChangeEvent<unknown>, value: number) {
         setPage(value);
-    }
+    };
 
     // 算頁數
     const indexOfLastPost = page * postsPerPage;
@@ -142,14 +134,17 @@ export default function Home() {
 
 
     return (
-        <div>
+        <div style={{ margin: '6em' }}>
             {status === "總攬" && (
-                <Grid sx={{ padding: 8 }}>
+                <div>
+                    {/*熱門文章*/}
                     <Typography variant="h2" component="div" sx={{ marginY: '0.5em', display: 'flex', alignItems: 'center', fontWeight: 'bold' }}>
                         <Whatshot sx={{ fontSize: '5rem', marginRight: '0.2em', color: 'indianred' }} />
                         ReactGOGO 熱門文章
                     </Typography>
-                    <Grid container spacing={2} >
+                    {/* 熱門文章-左半部顯示標題與按鈕、右半部顯示熱門文章 */}
+                    <Grid container>
+                        {/*熱門文章-介紹文字與查看所有熱門文章按鈕*/}
                         <Grid item xs={4} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                             <Typography variant="h4" component="div" sx={{ marginY: '1.5em', display: 'flex', alignItems: 'center' }}>
                                 最熱絡的討論<br /><br />　　就在 ReactGOGO
@@ -161,20 +156,26 @@ export default function Home() {
                                 </Typography>
                             </Button>
                         </Grid>
-                        {/*前三熱門*/}
-                        <Grid item direction="column" xs={8}>
+                        {/*熱門文章-前三名*/}
+                        <Grid item flexDirection="column" xs={8}>
                             {hot.map((post) => (postCard(post)))}
                         </Grid>
                     </Grid>
-                    <Divider light sx={{ marginY: '3em' }} />
+
+                    {/*分隔線*/}
+                    <Divider light sx={{ marginY: '4em' }} />
+
+                    {/*最新文章*/}
                     <Typography variant="h2" component="div" sx={{ marginY: '0.5em', display: 'flex', alignItems: 'center', fontWeight: 'bold' }}>
-                        <AutoAwesome sx={{ fontSize: '5rem', marginRight: '0.2em', color: 'Orange' }} />
-                        最新文章
+                        <AutoAwesome sx={{ fontSize: '5rem', marginRight: '0.2em', color: 'Orange' }} />最新文章
                     </Typography>
-                    <Grid container spacing={2} >
+
+                    {/* 最新文章-左半部顯示標題與按鈕、右半部顯示熱門文章 */}
+                    <Grid container>
+                        {/*最新文章-介紹文字與查看更多最新文章按鈕*/}
                         <Grid item xs={4} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                             <Typography variant="h4" component="div" sx={{ marginY: '1.5em', display: 'flex', alignItems: 'center' }}>
-                            ReactGOGO！<br /><br />　　新消息 GOGO！
+                                ReactGOGO！<br /><br />　　新消息 GOGO！
                             </Typography>
                             <img src="../Pleasant surprise-cuate.png" alt="welcome" width='100%' />
                             <Button sx={{ width: '85%', height: '30%', borderRadius: '1.5em', backgroundColor: 'Orange' }} variant="contained" onClick={() => changeStatus("本月")}>
@@ -183,21 +184,21 @@ export default function Home() {
                                 </Typography>
                             </Button>
                         </Grid>
-                        {/*前三本月*/}
-                        <Grid item direction="column" xs={8}>
+                        {/*最新文章-最新的三篇文章*/}
+                        <Grid item flexDirection="column" xs={8}>
                             {time.map((post) => (postCard(post)))}
                         </Grid>
                     </Grid>
-                </Grid>
+                </div>
             )}
 
             {status === "熱門" && (
-                <Grid sx={{padding: '4em'}}>
-                                        <Typography variant="h4" component="div" sx={{ marginY: '0.5em', display: 'flex', alignItems: 'center', fontWeight: 'bold' }}>
+                <Grid sx={{ padding: '4em' }}>
+                    <Typography variant="h4" component="div" sx={{ marginY: '0.5em', display: 'flex', alignItems: 'center', fontWeight: 'bold' }}>
                         <Whatshot sx={{ fontSize: '5rem', marginRight: '0.2em', color: 'indianred' }} />
                         所有熱門文章
                     </Typography>
-                    <Divider light/>
+                    <Divider light />
                     <Grid container spacing={2}>
                         {currentHot.map((post) => (smallPostCard(post)))}
                     </Grid>
@@ -215,7 +216,7 @@ export default function Home() {
             )}
 
             {status === "本月" && (
-                <Grid  sx={{padding: '4em'}}>
+                <Grid sx={{ padding: '4em' }}>
                     <Grid container spacing={2} sx={{ padding: 4 }}>
                         {currentTime.map((post) => (smallPostCard(post)))}
                     </Grid>
@@ -237,31 +238,43 @@ export default function Home() {
             {status === "詳細" && Id && (
                 <div>
                     {context.map((item) => (
-                        <div key={Id}>
-                            <div>{item.title}</div>
-                            <div>{item.account}</div>
-                            <div>{item.time.toDate().toLocaleString()}</div>
-                            <ReactQuill
-                                theme="snow"
-                                value={item.context}
-                                modules={{
-                                    toolbar: false,
-                                }}
-                                formats={[
-                                    'header',
-                                    'bold', 'italic', 'underline', 'strike', 'blockquote',
-                                    'list', 'bullet', 'indent',
-                                    'link', 'image'
-                                ]}
-                            />
-                            {/* <div>{item.context}</div> */}
-                            <div>{item.tag}</div>
-                            {item.photo && (
-                                <Image src={item.photo} alt="image" priority={true} height={300} width={300} />
-                            )}
-                        </div>
+                        <Card variant="outlined" sx={{ padding: '1em' }} key={Id}>
+                            <CardContent>
+                                <Typography variant="h6" component="div" sx={{ marginY: 1 }}>
+                                    {item.title}
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                    {item.account}
+                                </Typography>
+                                <Typography variant="body2">
+                                    {item.time.toDate().toLocaleString()}
+                                </Typography>
+                                <ReactQuill
+                                    theme="snow"
+                                    value={item.context}
+                                    modules={{
+                                        toolbar: false,
+                                    }}
+                                    formats={[
+                                        'header',
+                                        'bold', 'italic', 'underline', 'strike', 'blockquote',
+                                        'list', 'bullet', 'indent',
+                                        'link', 'image'
+                                    ]}
+                                />
+                                {item.tag.split(',').map((tagItem, index) => (
+                                    <Typography key={index}>
+                                        {tagItem.trim()} {/* 可能需要去除空格 */}
+                                    </Typography>
+                                ))}
+                                {item.photo && (
+                                    <Image src={item.photo} alt="image" priority={true} height={300} width={300} />
+                                )}
+                                <Button variant="outlined" onClick={() => changeStatus("總攬")}>返回總覽</Button>
+                            </CardContent>
+                        </Card>
                     ))}
-                    <Button variant="outlined" onClick={() => changeStatus("總攬")}>返回總覽</Button>
+
                 </div>
             )}
         </div>
