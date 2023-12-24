0656a18793371ae8d5971a9e4202667724adce28
葉文心 <WenHsin0130@gmail.com>
2023-12-24 14:51:04 +0800

    home page card ui

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
     // 熱門、最新文章 card（大）
     function postCard(post: { time: any; account: any; context: any; title: any; Id: any; like: number; isHeart: boolean; }, status: string) {
         return (
-            <Card variant="outlined" sx={{ padding: '1em' }}>
+            <Card variant="outlined" sx={{ padding: '1em', marginBottom: '0.8em' }}>
                 <CardContent>
                     <Typography variant="h4" component="div" sx={{ marginY: 1 }} fontWeight={'bold'}>
                         {post.title}
